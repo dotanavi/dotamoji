f3583e14e51572adc410a447fc967be7dd48d61e
Ota Daichi <daichi.ota@naviplus.co.jp>
2018-08-25 16:31:00 +0900

    refactoring

--- src/double_array.rs
+++ src/double_array.rs
@@ -1,4 +1,7 @@
-use std;
+use std::{char, u16, cmp::min, iter::once, fmt::Debug};
+
+#[derive(Eq, PartialEq)]
+enum Index { Ok, Empty, Conflict, OutOfRange }
 
 pub struct DoubleArray<T> {
     base: Vec<u32>,
@@ -21,8 +24,7 @@ impl<T> DoubleArray<T> {
     pub fn get(&self, key: &str) -> Option<&[T]> {
         let mut current_ix = 1;
         for ch in key.encode_utf16() {
-            let next_ix = self.base[current_ix] as usize + ch as usize;
-            if next_ix < self.check.len() && self.check[next_ix] as usize == current_ix {
+            if let (Index::Ok, next_ix) = self.next_index(current_ix, ch) {
                 current_ix = next_ix;
             } else {
                 return None;
@@ -41,8 +43,7 @@ impl<T> DoubleArray<T> {
         let mut current_ix = 1;
         for ch in key.encode_utf16() {
             chars.push(ch);
-            let next_ix = self.base[current_ix] as usize + ch as usize;
-            if next_ix < self.check.len() && self.check[next_ix] as usize == current_ix {
+            if let (Index::Ok, next_ix) = self.next_index(current_ix, ch) {
                 current_ix = next_ix;
                 if let Some(v) = self.data.get(current_ix) {
                     if v.len() > 0 {
@@ -53,78 +54,97 @@ impl<T> DoubleArray<T> {
         }
     }
 
-    pub fn insert(&mut self, key: &str, value: T) {
-        let (mut current_ix, key_ix) = self.find_failed_position(key);
-        for (ix, ch) in key.encode_utf16().enumerate() {
-            if ix < key_ix { continue; }
+    #[inline]
+    fn next_index(&self, current_index: usize, ch: u16) -> (Index, usize) {
+        let next_ix = self.base[current_index] as usize + ch as usize;
+        if next_ix < self.check.len() {
+            let check_ix = self.check[next_ix] as usize;
+            if check_ix == current_index {
+                (Index::Ok, next_ix)
+            } else if check_ix == 0 {
+                (Index::Empty, next_ix)
+            } else {
+                (Index::Conflict, next_ix)
+            }
+        } else {
+            (Index::OutOfRange, next_ix)
+        }
+    }
 
-            let current_base = self.base[current_ix] as usize;
-            let tmp_next_ix = current_base + ch as usize;
-            if tmp_next_ix < self.check.len() && self.check[tmp_next_ix] != 0 { // 衝突時
-                // 1. currIdx から遷移しているすべてのノード(遷移先ノード)を取得 (index, char)
-                let mut next_nodes = vec![];
-                for i in current_base .. current_base + std::u16::MAX as usize {
-                    if i >= self.check.len() { break; }
-                    if self.check[i] as usize == current_ix {
-                        next_nodes.push((i - current_base) as u16);
-                    }
+    pub fn insert(&mut self, key: &str, value: T) {
+        let mut current_ix = 1;
+        for ch in key.encode_utf16() {
+            let (state, next_ix) = self.next_index(current_ix, ch);
+            current_ix = match state {
+                Index::Ok => next_ix,
+                Index::Empty => {
+                    self.update(current_ix, next_ix)
                 }
-                // 2. 遷移先ノードと currChar が遷移可能なbaseを求める
-                let new_base = self.find_new_base(&next_nodes, ch);
-                self.base[current_ix] = new_base;
-                for ch in next_nodes {
-                    let src_ix = current_base + ch as usize;
-                    let src_base = self.base[src_ix] as usize;
-                    let dst_ix = self.base[current_ix] as usize + ch as usize;
-                    // 3. 遷移先ノードを新しい base で計算した index にコピー
-                    self.base[dst_ix] = self.base[src_ix];
-                    self.check[dst_ix] = self.check[src_ix];
-                    self.data.swap(src_ix, dst_ix);
-                    // 4. 旧遷移先ノードから更に遷移しているノードの check を新遷移先ノードの index で更新
-                    for i in src_base .. src_base + std::u16::MAX as usize {
-                        if i >= self.check.len() { break; }
-                        if self.check[i] as usize == src_ix {
-                            self.check[i] = dst_ix as u32;
-                        }
-                    }
-                    // 5. 旧遷移先ノードの base, check, data をリセット
-                    self.base[src_ix] = 1;
-                    self.check[src_ix] = 0;
-                    debug_assert!(self.data[src_ix].len() == 0);
-                }
-            }
-            // currChar のノードを追加
-            let next_ix = self.base[current_ix] as usize + ch as usize;
-            if next_ix >= self.check.len() {
-                self.extend(next_ix + 1);
-            }
-            self.base[next_ix] = 1;
-            self.check[next_ix] = current_ix as u32;
-            current_ix = next_ix;
+                Index::Conflict => {
+                    let new_next_ix = self.rebase(current_ix, ch);
+                    self.update(current_ix, new_next_ix)
+                },
+                Index::OutOfRange => {
+                    self.extend(next_ix + 1);
+                    self.update(current_ix, next_ix)
+                },
+            };
         }
         // データを登録
         self.data[current_ix].push(value);
     }
 
-    fn find_failed_position(&self, key: &str) -> (usize, usize) {
-        let mut current_ix = 1;
-        for (ix, ch) in key.encode_utf16().enumerate() {
-            let next_ix = self.base[current_ix] as usize + ch as usize;
-            if next_ix < self.check.len() && self.check[next_ix] as usize == current_ix {
-                current_ix = next_ix;
-            } else {
-                return (current_ix, ix)
-            }
-        }
-        return (current_ix, key.encode_utf16().count())
+    #[inline]
+    fn update(&mut self, current_ix: usize, next_ix: usize) -> usize {
+        self.base[next_ix] = 1;
+        self.check[next_ix] = current_ix as u32;
+        next_ix
     }
 
-    fn find_new_base(&mut self, next_nodes: &[u16], ch: u16) -> u32 {
+    fn rebase(&mut self, current_ix: usize, ch: u16) -> usize {
+        let current_base = self.base[current_ix] as usize;
+        // 1. currIdx から遷移しているすべてのノード(遷移先ノード)を取得 (index, char)
+        let mut next_nodes = vec![];
+        for i in current_base .. min(self.check.len(), current_base + u16::MAX as usize) {
+            if self.check[i] as usize == current_ix {
+                next_nodes.push((i - current_base) as u16);
+            }
+        }
+        // 2. 遷移先ノードと currChar が遷移可能なbaseを求める
+        let new_base = self.find_new_base(&next_nodes, ch);
+        self.base[current_ix] = new_base as u32;
+        for ch in next_nodes {
+            let src_ix = current_base + ch as usize;
+            let src_base = self.base[src_ix] as usize;
+            let dst_ix = new_base as usize + ch as usize;
+
+            debug_assert!(self.base[dst_ix] == 0);
+            debug_assert!(self.check[dst_ix] == 0);
+            debug_assert!(self.data[dst_ix].len() == 0);
+            // 3. 遷移先ノードを新しい base で計算した index にコピー
+            self.base[dst_ix] = self.base[src_ix];
+            self.check[dst_ix] = self.check[src_ix];
+            self.data.swap(src_ix, dst_ix);
+
+            // 4. 旧遷移先ノードから更に遷移しているノードの check を新遷移先ノードの index で更新
+            for i in src_base .. min(self.check.len(), src_base + u16::MAX as usize) {
+                if self.check[i] as usize == src_ix {
+                    self.check[i] = dst_ix as u32;
+                }
+            }
+            // 5. 旧遷移先ノードの base, check, data をリセット
+            self.base[src_ix] = 0;
+            self.check[src_ix] = 0;
+        }
+        new_base as usize + ch as usize
+    }
+
+    fn find_new_base(&mut self, next_nodes: &[u16], ch: u16) -> usize {
         let mut new_base = 0;
         'out: loop {
             new_base += 1;
-            for ch in next_nodes.iter().chain(std::iter::once(&ch)) {
-                let new_ix = new_base as usize + *ch as usize;
+            for ch in next_nodes.iter().chain(once(&ch)) {
+                let new_ix = new_base + *ch as usize;
                 if new_ix < self.check.len() {
                     if self.check[new_ix] == 0 {
                         // OK
@@ -153,11 +173,11 @@ impl<T> DoubleArray<T> {
     }
 }
 
-impl<T: std::fmt::Debug> DoubleArray<T> {
+impl<T: Debug> DoubleArray<T> {
     pub fn show_debug(&self) {
         for i in 0 .. self.check.len() {
             if self.base[i] == 0 { continue; }
-            let ch = std::char::from_u32(i as u32 - self.base[self.check[i] as usize] as u32);
+            let ch = char::from_u32(i as u32 - self.base[self.check[i] as usize] as u32);
             println!("{}\t, {}\t, {}\t, {}\t, {:?}",
                 i, self.base[i], self.check[i], ch.unwrap(), self.data[i]);
         }
@@ -165,7 +185,7 @@ impl<T: std::fmt::Debug> DoubleArray<T> {
 }
 
 fn decode_utf16(chars: &[u16]) -> String {
-    std::char::decode_utf16(chars.iter().cloned())
+    char::decode_utf16(chars.iter().cloned())
         .filter_map(Result::ok)
         .collect()
 }
