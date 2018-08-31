dbda261df2fd186b6820c7252244ba9976cb754c
Ota Daichi <daichi.ota@naviplus.co.jp>
2018-08-31 22:56:35 +0900

    optimize

--- src/transform.rs
+++ src/transform.rs
@@ -96,20 +96,22 @@ pub fn transform<T>(trie: Trie<T>) -> DoubleArray<T> {
 }
 
 #[inline]
-fn find_base<T>(check: &[u32], children: &[(u16, T)]) -> usize {
-    if children.len() == 1 {
-        let ch = children[0].0 as usize;
-        let mut ix = ch + 1;
-        while ix < check.len() && check[ix] != 0 {
-            ix += 1;
-        }
-        return ix - ch;
+fn find_base_one(ch: usize, search_start: usize, check: &[u32]) -> usize {
+    let mut ix = ch + search_start + 1;
+    while ix < check.len() && check[ix] != 0 {
+        ix += 1;
     }
+    return ix - ch;
+}
+
+#[inline]
+fn find_base<T>(check: &[u32], children: &[(u16, T)]) -> usize {
+    let ch = children[0].0 as usize;
 
     let mut index = 0;
     'outer: loop {
-        index += 1;
-        for &(ch, _) in children {
+        index = find_base_one(ch, index, check);
+        for &(ch, _) in &children[1..] {
             let ix = index + ch as usize;
             if ix < check.len() && check[ix] != 0 {
                 continue 'outer;
@@ -143,7 +145,7 @@ fn put_rec<T>(mut node: Node<T>, base_index: usize, base: &mut Vec<u32>, check: 
         extend(check, requred_size);
         extend(data, requred_size);
     }
-    for &(ch, _) in node.children.iter() {
+    for &(ch, _) in &node.children {
         check[new_base + ch as usize] = base_index as u32;
     }
     for (ch, child_node) in node.children {
