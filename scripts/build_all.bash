0ca9bf70991d549c19f03a3af581a3615c4dc966
Ota Daichi <daichi.ota@naviplus.co.jp>
2018-09-02 11:23:24 +0900

    fix script

--- scripts/build_all.bash
+++ scripts/build_all.bash
@@ -12,14 +12,16 @@ save_dir=tmp
 function build () {
   local build_type="$1"
   local binary_format="${2:-$1}"
+  local dic_path="$save_dir/${build_type}_all.dic"
 
   echo
   echo "----------------------------------------------------------------"
   echo
   (
     set -x
-    time "$out_dir/build-dict" "$build_type" "$save_dir/${build_type}_all.dic" < $dic_file
-    time "$out_dir/test-dict" "$binary_format" "$save_dir/${build_type}_all.dic" < $dic_file
+    time "$out_dir/build-dict" "$build_type" "$dic_path.tmp" < $dic_file
+    time "$out_dir/test-dict" "$binary_format" "$dic_path.tmp" < $dic_file
+    mv "$dic_path.tmp" "$dic_path"
   )
 }
 
