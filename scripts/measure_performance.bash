c2311b4e318bae1a426e2022100273a4480063a6
Ota Daichi <daichi.ota@naviplus.co.jp>
2018-09-10 22:03:11 +0900

    update measurement script

--- scripts/measure_performance.bash
+++ scripts/measure_performance.bash
@@ -7,34 +7,53 @@ cd "$(dirname $0)/.."
 out_dir=./target/release
 dic_file=../dic/All.csv
 
+# -- names
+declare -A encoding;
+encoding=(
+  ["8"]="UTF-8"
+  ["16"]="UTF-16"
+  ["32"]="UTF-32"
+)
+
+declare -A structure;
+structure=(
+  ["array"]="ダブル配列"
+  ["trie"]="トライ木"
+  ["hash"]="再帰HashMap"
+  ["fast"]="ダブル配列（空き要素をビットマップから検索）"
+  ["trans"]="トライ木で構築し、ダブル配列に変換"
+)
+
+
 # -- functions
 function measure () {
-  local type="$1"
-  local enc="$2"
-  local desc="$3"
+  local struct="${structure[$1]}"
+  local enc="${encoding[$2]}"
+  local type="${1}${2}"
   local time=$( (time $out_dir/build-dict $type - < $dic_file > /dev/null) 2>&1 | grep real | cut -f 2 )
-  echo "| $type | $enc | $desc | $time |"
+  echo "| $type | $struct | $enc | $time |"
 }
 
+# -- main
 cargo build --release --quiet
 
-echo "| オプション | 文字コード | データ構造 | 構築時間 |"
+echo "| オプション | データ構造 | 文字コード | 構築時間 |"
 echo "|--|--|--|--:|"
 
-measure array8  "UTF-8"          "ダブル配列"
-measure array16 "UTF-16"         "ダブル配列"
+measure array 8
+measure array 16
 
-measure trie8  "UTF-8"           "トライ木"
-measure trie16 "UTF-16"          "トライ木"
-measure trie32 "ユニコードポイント" "トライ木"
+measure trie 8
+measure trie 16
+measure trie 32
 
-measure hash8  "UTF-8"           "再帰HashMap"
-measure hash16 "UTF-16"          "再帰HashMap"
-measure hash32 "ユニコードポイント" "再帰HashMap"
+measure hash 8
+measure hash 16
+measure hash 32
 
-measure fast8  "UTF-8"            "ビットマップキャッシュを用いてダブル配列を構築"
-measure fast16 "UTF-16"           "ビットマップキャッシュを用いてダブル配列を構築"
+measure fast 8
+measure fast 16
 
-measure trans8  "UTF-8"           "トライ木を構築し、ダブル配列に変換"
-measure trans16 "UTF-16"          "トライ木を構築し、ダブル配列に変換"
-measure trans32 "ユニコードポイント" "トライ木を構築し、ダブル配列に変換"
+measure trans 8
+measure trans 16
+measure trans 32
