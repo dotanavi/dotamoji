a6b8af91285c6a7a206be259148971972a3390cf
Ota Daichi <daichi.ota@naviplus.co.jp>
2018-09-11 15:07:09 +0900

    fix test.bash

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-src_type="fast16"
+src_type="trans16"
 dst_type="array16"
-src="../dic/Noun.csv"
-dst="tmp/foo.bin"
+src="../dic/All.csv"
+dst="tmp/tmp.bin"
 
 set -Ceux
 
 cargo build --release
-time ./target/release/build-dict "$src_type" "$dst" < "$src"
+./target/release/build-dict "$src_type" "$dst" < "$src"
 ./target/release/test-dict "${dst_type:-$src_type}" "$dst" < "$src"
